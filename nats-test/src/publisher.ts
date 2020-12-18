5043c00c43dfe68b16c12feb1eab92abd9767805
vmmaia <vasco.matos.maia@gmail.com>
2020-12-18 13:12:25 +0000

    Created NATS interface

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -6,16 +7,18 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Pub connected to nats');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'title',
-    price: 20
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  stan.publish('ticket:created', data, () => {
-    console.log('event published');
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'Title',
+      price: 20
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
