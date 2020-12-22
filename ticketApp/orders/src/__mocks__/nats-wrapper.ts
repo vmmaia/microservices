089e7d3f6833790180a9966d2a228fbbddad86ac
vmmaia <vasco.matos.maia@gmail.com>
2020-12-22 19:48:00 +0000

    finished section 18

---
+++ ticketApp/orders/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,13 @@
+import { createJSDocCallbackTag } from 'typescript';
+
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      )
+  }
+};
